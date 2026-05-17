66475323c388beb8530088a7c60e4301ad54b2b9
SAMSAM5.5 <sami.saubion@gmail.com>
2026-05-17 22:10:56 +0200

    fix: small bugs with API and frontend rendering

--- app/coffee/page.tsx
+++ app/coffee/page.tsx
@@ -1,9 +1,9 @@
 "use client";
 
-import { useEffect } from "react";
+import { ReactElement, useEffect } from "react";
 import { goralysFetchClient } from "@/app/lib/fetch/fetch.client";
 
-export default function CoffeePage(): void {
+export default function CoffeePage(): ReactElement {
     useEffect(() => {
         goralysFetchClient("coffee", {
             method: "POST",
@@ -12,4 +12,6 @@ export default function CoffeePage(): void {
             },
         }).then();
     }, []);
+
+    return <></>;
 }
