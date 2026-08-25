05a1df2ad80dc13adcd9d813d5fd7f49cce38830
SAMSAM-55 <sami.saubion@dev.goralys.fr>
2026-08-25 23:17:31 +0200

    feat: published goralys/core@1.6.2

--- goralys-core/src/lib/fetch/fetch.client.ts
+++ goralys-core/src/lib/fetch/fetch.client.ts
@@ -43,7 +43,7 @@ export async function goralysFetchClient(
 
     const data = await clone.json();
 
-    if (data?.redirect && requestOptions?.suppressRedirect) {
+    if (data?.redirect && !requestOptions?.suppressRedirect) {
         emitNavigationEvent({ type: "redirect", url: data.redirect });
     }
 
